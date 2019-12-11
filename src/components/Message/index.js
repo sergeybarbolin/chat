fb2e137a6b10eec1bba49fd16b84cf789420210b
Sergey Barbolin <sergeybarbolin25@gmail.com>
2019-12-11 18:17:23 +0300

    message styling

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,18 +1,44 @@
 import React from 'react'
 import PropTypes from 'prop-types';
-import classNames from 'classnames'
-import './Message.scss';
+import { formatDistanceToNow } from 'date-fns';
+import ruLocale from 'date-fns/locale/ru'
+import classNames from 'classnames';
 
-const Message = ({ avatar, user, text, date }) => (
-    <div className="message">
-        <div className="message__avater">
+import './Message.scss';
+import readedSvg from './../../assets/img/readed.svg';
+import noReadedSvg from './../../assets/img/noreaded.svg';
+
+const Message = ({ avatar, user, text, date, isMe, readed, attachments }) => (
+    <div className={ classNames('message', { "message--isme": isMe })}>
+        <div className="message__avatar">
             <img src={avatar} alt={user.fullname} />
         </div>
         <div className="message__content">
             <div className="message__bubble">
                 <p className="message__text">{text}</p>
             </div>
-            <span className="message__date">Вчера в 12:31</span>
+            { attachments && 
+                <div className="message__attachments">
+                    {
+                        attachments.map(item => (
+                            <div className="message__attachments-item">
+                                <img src={item.url} alt="" />
+                            </div>
+                        ))
+                    }
+                </div> 
+            }
+            <div className="message__info">
+                { isMe && <img className="message__status" src={ readed ? readedSvg : noReadedSvg  } alt="message-status" /> }
+                <span className="message__date">
+                    { formatDistanceToNow(new Date( date ), 
+                        {
+                            addSuffix: true,
+                            locale: ruLocale
+                        }
+                    )}
+                </span>
+            </div>
         </div>
     </div>
 )
@@ -26,6 +52,9 @@ Message.propTypes = {
     text: PropTypes.string,
     date: PropTypes.string,
     user: PropTypes.object,
+    attachments: PropTypes.array,
+    isMe: PropTypes.bool,
+    readed: PropTypes.bool
 }
 
 export default Message
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -7,8 +7,22 @@ const Home = () => (
     <section className="home">
         <Message 
             avatar="https://zolotoy.ru/wp-content/uploads/2019/10/Ntv8QRMb-Wo-e1571144474635-254x300.jpg"
-            text="Hello!"
-            date="Thu Nov 28 2019 10:19:12 GMT+0300"
+            text="Ex laboris deserunt tempor consequat esse reprehenderit. Anim nisi qui nisi nostrud id qui et sint ullamco irure laboris laborum amet incididunt. Eu labore exercitation cillum ullamco. Culpa nulla sint est deserunt officia adipisicing do ullamco."
+            date="Thu Nov 28 2019 10:19:12"
+            isMe={false}
+        />
+        <Message 
+            avatar="https://zolotoy.ru/wp-content/uploads/2019/10/DSCF6760-e1571049298553-278x300.jpg"
+            text="Enim id occaecat reprehenderit proident."
+            date="Thu Dec 11 2019 15:32:12"
+            isMe={true}
+            readed={true}
+            attachments={[
+                { url: 'https://source.unsplash.com/200x200/?random=1' },
+                { url: 'https://source.unsplash.com/200x200/?random=2' },
+                { url: 'https://source.unsplash.com/200x200/?random=3' },
+                { url: 'https://source.unsplash.com/200x200/?random=4' },
+            ]}
         />
     </section>
 );
